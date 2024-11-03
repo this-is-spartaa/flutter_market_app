b701ca29d2557239aad5bfd5332792d4b4aac845
111coding <jwlee1024@gmail.com>
2024-11-03 22:45:33 +0900

    feat: WelcomePage UI

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        // 나중에 앱 메인 색상 쓸 때를 대비하여 정의!
+        highlightColor: Colors.orange,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            // 버튼 내 텍스트나 아이콘 위젯 색상
+            foregroundColor: WidgetStatePropertyAll(Colors.white),
+            backgroundColor: WidgetStatePropertyAll(Colors.orange),
+            // 사이즈 설정. 안하면 ElevatedButton은 자녀 위젯의 크기로 축소됨!
+            minimumSize: WidgetStatePropertyAll(Size.fromHeight(50)),
+            textStyle: WidgetStatePropertyAll(
+              TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            // BorderRadius 설정!
+            shape: WidgetStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+        ),
+      ),
       home: WelcomePage(),
     );
   }
--- lib/ui/pages/welcome/welcome_page.dart
+++ lib/ui/pages/welcome/welcome_page.dart
@@ -1,11 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_market_app/ui/pages/address_search/address_search_page.dart';
+import 'package:flutter_market_app/ui/pages/login/login_page.dart';
 
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text('WelcomePage'),
+      // AppBar를 사용하지 않기 때문에 SafeArea 사용!!!
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          // Column의 기본 너비는 chlidred 중 가장 넓은 위젯의 너비. 글자 가로 중앙정렬을 위해 너비 디바이스 너비로!
+          child: SizedBox.expand(
+            child: Column(
+              children: [
+                Spacer(),
+                Image.asset(
+                  "assets/welcome.png",
+                  height: 250,
+                ),
+                Text(
+                  "당신 근처의 마켓",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "동네라서 가능한 모든것\n지금 내 동네를 선택하고 시작해보세요!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                Spacer(),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return AddressSearchPage();
+                      },
+                    ));
+                  },
+                  child: Text("시작하기"),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return LoginPage();
+                      },
+                    ));
+                  },
+                  child: Container(
+                    height: 50,
+                    color: Colors.transparent,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '이미 계정이 있나요? ',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        Text(
+                          '로그인',
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
