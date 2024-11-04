59049b3ac900e95e301de77c0e351cdb8d1f61ea
111coding <jwlee1024@gmail.com>
2024-11-04 20:01:23 +0900

    hotFix: ChatDetailPage AppBar title nickname

--- lib/ui/pages/chat_detail/chat_detail_page.dart
+++ lib/ui/pages/chat_detail/chat_detail_page.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_market_app/ui/chat_global_view_model.dart';
 import 'package:flutter_market_app/ui/pages/chat_detail/widgets/chat_detail_bottom_sheet.dart';
 import 'package:flutter_market_app/ui/pages/chat_detail/widgets/chat_detail_list_view.dart';
 import 'package:flutter_market_app/ui/pages/chat_detail/widgets/chat_detail_product_area.dart';
+import 'package:flutter_market_app/ui/user_global_view_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ChatDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('오상구'),
+        title: Consumer(
+          builder: (context, ref, child) {
+            final chatRoom = ref.watch(chatGlobalViewModel).currentChat;
+            final user = ref.watch(userGlobalViewModel);
+            final nickname = chatRoom?.sender.id == user?.id
+                ? chatRoom?.product.user.nickname
+                : chatRoom?.sender.nickname;
+            return Text(nickname ?? '');
+          },
+        ),
         centerTitle: true,
       ),
       bottomSheet: ChatDetailBottomSheet(
